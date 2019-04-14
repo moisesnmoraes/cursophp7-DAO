d23e796ed815e94471e813c1d9c74ede4b8347f1
moises n maoraes <moisesnmoraes@outlook.com>
2019-04-14 14:30:07 -0300

    inserir e alterar

--- class/sql.php
+++ class/sql.php
@@ -1,54 +1,52 @@
 <?php 
 
-class Sql extends PDO{
+class Sql extends PDO {
 
 	private $conn;
 
 	public function __construct(){
 
-		 $this->conn = new PDO("mysql:host=localhost;dbname=dbphp7","root","");
-	}
-
-	private function setParams($statment, $parameters=array()){
-
-			foreach ($parameters as $key => $value) {
-				
-				$this->setParam($statment,$key, $value);
-
-
-
-			}
-
+		$this->conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root", "");
 
 	}
 
-	private function setParam($statment, $key, $value){
-		$statment->bindParam($key, $value);
+	private function setParams($statement, $parameters = array()){
 
+		foreach ($parameters as $key => $value) {
+			
+			$this->setParam($statement, $key, $value);
+
+		}
+
+	}
+
+	private function setParam($statement, $key, $value){
+
+		$statement->bindParam($key, $value);
 
 	}
 
 	public function query($rawQuery, $params = array()){
 
-			$stmt = $this->conn->prepare($rawQuery);
+		$stmt = $this->conn->prepare($rawQuery);
 
+		$this->setParams($stmt, $params);
 
-			$this->setParams($stmt, $params);
+		$stmt->execute();
 
-			$stmt->execute();
-			return $stmt;
+		return $stmt;
 
 	}
 
-	public function select($rawQuery, $params =array()):array
+	public function select($rawQuery, $params = array()):array
 	{
-		$stmt = $this->query($rawQuery, $params);
-		return $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+		$stmt = $this->query($rawQuery, $params);
+
+		return $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 	}
 
 }
-
 
  ?>
--- index.php
+++ index.php
@@ -1,16 +1,47 @@
-<?php  
+<?php 
+
 require_once("config.php");
 
-//$sql= new Sql();
-
-//$usuarios = $sql->select("SELECT * FROM  tb_usuarios");
-
-
-//echo json_encode($usuarios); 
+//Carrega um usuário
 $root = new Usuario();
-
-$root->loadById(3);
-
+$root->loadbyId(3);
 echo $root;
 
+//Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+//Carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search("jo");
+//echo json_encode($search);
+
+//carrega um usuário usando o login e a senha
+//$usuario = new Usuario();
+//$usuario->login("root", "");
+
+//echo $usuario;
+/*
+//Criando um novo usuário
+$aluno = new Usuario("aluno", "@lun0");
+$aluno->insert();
+echo $aluno;
+*/
+/*
+//Alterar um usuário
+$usuario = new Usuario();
+
+$usuario->loadById(8);
+
+$usuario->update("professor", "!@#$%¨&*");
+
+echo $usuario;
+*/
+
+$usuario = new Usuario();
+
+$usuario->loadById(7);
+
+$usuario->delete();
+
+echo $usuario;
  ?>
